8c341af90252a75b4f9e22fd6725f0c8bd653039
scarl <scarlet25151@gmail.com>
2021-05-08 17:44:22 -0400

    Update directory

---
+++ dataset/download.sh
@@ -0,0 +1,3 @@
+!TODO: to be replace by the new download link
+wget --no-check-certificate https://drive.google.com/file/d/1bEoHXqUTuHDYdpGrB6p-VHV2HS81f0Pw/view?usp=sharing
+wget --no-check-certificate https://drive.google.com/file/d/1-0Uzz6QIkhEQv0UQvOJi1xwVUKGXSdi0/view?usp=sharing
