45254dce961cf985b22d32a7b59855d7528eae58
scarl <scarlet25151@gmail.com>
2021-05-10 14:42:34 -0400

    remove .idea

---
+++ utils/data_process.py
@@ -0,0 +1,44 @@
+from io import StringIO
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+
+from .inputs import SparseFeat, DenseFeat, get_feature_names
+
+
+def process_input(path, encoding='utf-8', use_densefeat=True):
+    with open(path, encoding='utf-8') as f:
+        contents = f.read()
+
+    names = ['label', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13',
+             'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14',
+             'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21', 'C22',
+             'C23', 'C24', 'C25', 'C26']
+    data = pd.read_csv(StringIO(contents), sep='\t', header=None, names=names)
+
+    sparse_features = ['C' + str(i) for i in range(1, 27)]
+    dense_features = ['I' + str(i) for i in range(1, 14)]
+
+    data[sparse_features] = data[sparse_features].fillna('-1', )
+    data[dense_features] = data[dense_features].fillna(0, )
+
+    for feat in sparse_features:
+        lbe = LabelEncoder()
+        data[feat] = lbe.fit_transform(data[feat])
+    mms = MinMaxScaler(feature_range=(0, 1))
+    data[dense_features] = mms.fit_transform(data[dense_features])
+
+    if use_densefeat:
+        fixlen_feature_columns = [SparseFeat(feat, vocabulary_size=data[feat].max() + 1, embedding_dim=4)
+                                  for i, feat in enumerate(sparse_features)] + [DenseFeat(feat, 1, )
+                                                                                for feat in dense_features]
+    else:
+        fixlen_feature_columns = [SparseFeat(feat, vocabulary_size=data[feat].max() + 1, embedding_dim=4)
+                                  for i, feat in enumerate(sparse_features)]
+
+    dnn_feature_columns = fixlen_feature_columns
+    linear_feature_columns = fixlen_feature_columns
+
+    feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
+
+    return data, feature_names
